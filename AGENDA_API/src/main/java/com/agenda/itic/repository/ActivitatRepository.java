02b0fd7608e09d2798fce66653ff14f589933eca
Ian <baorii310@gmail.com>
2026-03-17 16:05:13 +0100

    quitado sobrante api

--- AGENDA_API/src/main/java/com/agenda/itic/repository/ActivitatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agenda.itic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.agenda.itic.model.Activitat;
-
-@Repository
-public interface ActivitatRepository extends JpaRepository<Activitat, Long> {
-}
--- AGENDA_API/src/main/java/com/agenda/itic/repository/DispositiuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agenda.itic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.agenda.itic.model.Dispositiu;
-
-@Repository
-public interface DispositiuRepository extends JpaRepository<Dispositiu, Long> {
-}
